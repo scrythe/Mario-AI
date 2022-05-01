e2b8335b67cad603803f971c238e4b17508a9a97
Magomed <scrythepvp@gmail.com>
2022-05-01 18:14:16 +0200

    feat: add rescaling

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import retro
 import neat
 import os
+import cv2
+import numpy
 
 
-def start_game():
+def start_game(genome: neat.DefaultGenome, net: neat.nn.RecurrentNetwork):
     env = retro.make(game='SuperMarioWorld-Snes', state='YoshiIsland1.state')
-    obs = env.reset()
+    screen = env.reset()
+
+    width, height, color = env.observation_space.shape
+    scaled_width = int(width/8)
+    scaled_height = int(height/8)
+    # cv2.namedWindow("main", cv2.WINDOW_NORMAL)
+
     done = False
     while not done:
         env.render()
 
+        resized_screen = cv2.resize(screen, (scaled_width, scaled_height))
+        recolored_screen = cv2.cvtColor(resized_screen, cv2.COLOR_BGR2GRAY)
+        reshaped_screen = numpy.reshape(
+            recolored_screen, (scaled_width, scaled_height))
+        # cv2.imshow('main', recolored_screen)
+        # cv2.waitKey(1)
+
+        action = env.action_space.sample()
+        screen, reward, done, info = env.step(action)
+
 
 def eval_genomes(genomes, config):
     for genome_id, genome in genomes:
-        start_game()
+        net = neat.nn.RecurrentNetwork.create(genome, config)
+        start_game(genome, net)
 
 
 def run_neat(config_file):
